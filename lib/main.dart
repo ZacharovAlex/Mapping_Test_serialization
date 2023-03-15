9407c5835f3d139c17edc0741c8254f1b41b8b10
Alex <zacharov.lao@gmail.com>
2023-03-16 00:27:52 +0300

    Change map function

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
-  final List<String> nav = ['Мои фото', 'Галерея'];
-  Map<String, dynamic> data = {
+  Map<String, List<String>> data = {
     'Мои фото': [
       'https://picsum.photos/1200/501',
       'https://picsum.photos/1200/502',
@@ -52,45 +51,18 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: nav.length,
+        length: data.length,
         child: Scaffold(
           appBar: AppBar(
             title: const Text('Homework example'),
           ),
           body: TabBarView(
-            children: nav.map((name) {
-              return ListView(
-                key: PageStorageKey(name),
-                children: <Widget>[
-                  ...(data[name] as List<String>).map((image) {
-                    return Image.network(image);
-                  }).toList()
-                ],
-              );
-            }).toList(),
-          ),
+              children: data.entries.map((e) {
+            return ListView(
+              key: PageStorageKey(e.key),
+              children: <Widget>[for (var image in e.value) Image.network(image)],
+            );
+          }).toList()),
         ));
   }
 }
-
-// class ListOfLinks {
-//   List<String> images;
-//
-//   ListOfLinks({required this.images});
-//
-//   factory ListOfLinks.fromJson(Map<String, dynamic> json, String galeryName) {
-//     return ListOfLinks(images: json[galeryName] as List<String>);
-//   }
-// }
-
-// class ImageList extends StatelessWidget {
-//   final List<String> imageString;
-//   const ImageList({Key? key, required this.imageString}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(children: [
-//       ...imageString.map((image) => Image.network(image)).toList()
-//     ],);
-//   }
-// }
